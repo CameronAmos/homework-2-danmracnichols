77aff8b28f1d8c9f9fbc8c4f72ffff5f1fdcba94
Daniel Nichols <danmarcnichols@gmail.com>
2018-09-11 13:00:04 -0500

    "Added the proper function"

--- count\_files\_and\_subdirs.sh
+++ count\_files\_and\_subdirs.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-find -type d -or -type f | grep -v '^.$' | wc -l
+
+#find -type d -or -type f | grep -v '^.$' | wc -l
+echo "The number of files and subdirectories are:"
+find . -type f | wc -l
+
+
