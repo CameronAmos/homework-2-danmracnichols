0a64ce5e9eea3972d0a39a5d4ae091019a380fd7
danmracnichols <danmarcnichols@gmail.com>
2018-09-11 12:51:13 -0500

    Update temperature.sh

--- temperature.sh
+++ temperature.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 F=$1
 C=$(echo "scale=2;($F-32)*5/9" | bc)
-echo "$F degrees Fahreheit is equivalent to $C degrees Celsius"
+echo "$F degrees Fahreheit is the same as $C degrees Celsius"
