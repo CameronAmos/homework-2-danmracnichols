e7f7edbe8ab2eed6de2a9c1fd6018700bf484b09
Cameron Amos <camos333@ksu.edu>
2018-09-12 00:43:36 -0500

    This is cameron, adding Kelvin equation onto temperature.sh on new branch dev

--- temperature.sh
+++ temperature.sh
@@ -1,4 +1,8 @@
+
 #!/bin/bash
 F=$1
 C=$(echo "scale=2;($F-32)*5/9" | bc)
 echo "$F degrees Fahreheit is the same as $C degrees Celsius"
+
+K=$(echo "scale=2;($C+273.15)" | bc)
+echo "$F degrees Fahreheit is the same as $K degrees Kelvin"
